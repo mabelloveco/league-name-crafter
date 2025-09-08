e5c8898c4d4ea09f1468dc2ffdc5ec1756a5d7a8
mabelloveco <188736915+mabelloveco@users.noreply.github.com>
2025-09-08 14:48:28 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -29,21 +29,26 @@ const App = () => (
           <div className="min-h-screen flex flex-col">
             <Navigation />
             <main className="flex-1">
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/generator" element={<Generator />} />
-                <Route path="/funny-names" element={<FunnyNames />} />
-                <Route path="/inappropriate-names" element={<InappropriateNames />} />
-                <Route path="/player-themed-names" element={<PlayerThemedNames />} />
-                <Route path="/pop-culture-names" element={<PopCultureNames />} />
-                
-                {/* BLOG ROUTES */}
-                <Route path="/blog" element={<BlogList />} />
-                <Route path="/blog/:slug" element={<BlogPost />} />
+             <Routes>
+  <Route path="/" element={<HomePage />} />
 
-                {/* CATCH-ALL: redirect unknown routes to home */}
-                <Route path="*" element={<Navigate to="/" replace />} />
-              </Routes>
+  {/* FAQ replaces Generator */}
+  <Route path="/faq" element={<FAQ />} />
+  <Route path="/generator" element={<Navigate to="/faq" replace />} />
+
+  <Route path="/funny-names" element={<FunnyNames />} />
+  <Route path="/inappropriate-names" element={<InappropriateNames />} />
+  <Route path="/player-themed-names" element={<PlayerThemedNames />} />
+  <Route path="/pop-culture-names" element={<PopCultureNames />} />
+
+  {/* BLOG ROUTES */}
+  <Route path="/blog" element={<BlogList />} />
+  <Route path="/blog/:slug" element={<BlogPost />} />
+
+  {/* CATCH-ALL: redirect unknown routes to home */}
+  <Route path="*" element={<Navigate to="/" replace />} />
+</Routes>
+
             </main>
             <Footer />
           </div>
