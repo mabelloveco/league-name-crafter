45c041f2d2a527edc88b5341b32789b0dd25c2a8
mabelloveco <188736915+mabelloveco@users.noreply.github.com>
2025-09-08 14:54:02 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,16 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { Navigation } from "@/components/Navigation";
 import { Footer } from "@/components/Footer";
+
+// Pages
 import HomePage from "./pages/HomePage";
-import Generator from "./pages/Generator";
+import FAQ from "./pages/FAQ"; // ✅ new FAQ page
 import FunnyNames from "./pages/FunnyNames";
 import InappropriateNames from "./pages/InappropriateNames";
 import PlayerThemedNames from "./pages/PlayerThemedNames";
 import PopCultureNames from "./pages/PopCultureNames";
 
-// NEW: blog pages
+// Blog pages
 import BlogList from "./pages/BlogList";
 import BlogPost from "./pages/BlogPost";
 
@@ -29,26 +31,25 @@ const App = () => (
           <div className="min-h-screen flex flex-col">
             <Navigation />
             <main className="flex-1">
-             <Routes>
-  <Route path="/" element={<HomePage />} />
+              <Routes>
+                <Route path="/" element={<HomePage />} />
 
-  {/* FAQ replaces Generator */}
-  <Route path="/faq" element={<FAQ />} />
-  <Route path="/generator" element={<Navigate to="/faq" replace />} />
+                {/* ✅ FAQ replaces Generator */}
+                <Route path="/faq" element={<FAQ />} />
+                <Route path="/generator" element={<Navigate to="/faq" replace />} />
 
-  <Route path="/funny-names" element={<FunnyNames />} />
-  <Route path="/inappropriate-names" element={<InappropriateNames />} />
-  <Route path="/player-themed-names" element={<PlayerThemedNames />} />
-  <Route path="/pop-culture-names" element={<PopCultureNames />} />
+                <Route path="/funny-names" element={<FunnyNames />} />
+                <Route path="/inappropriate-names" element={<InappropriateNames />} />
+                <Route path="/player-themed-names" element={<PlayerThemedNames />} />
+                <Route path="/pop-culture-names" element={<PopCultureNames />} />
 
-  {/* BLOG ROUTES */}
-  <Route path="/blog" element={<BlogList />} />
-  <Route path="/blog/:slug" element={<BlogPost />} />
+                {/* ✅ Blog routes */}
+                <Route path="/blog" element={<BlogList />} />
+                <Route path="/blog/:slug" element={<BlogPost />} />
 
-  {/* CATCH-ALL: redirect unknown routes to home */}
-  <Route path="*" element={<Navigate to="/" replace />} />
-</Routes>
-
+                {/* Catch-all → home */}
+                <Route path="*" element={<Navigate to="/" replace />} />
+              </Routes>
             </main>
             <Footer />
           </div>
